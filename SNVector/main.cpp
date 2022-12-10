a21c5204c29379c808e9a3d734cbed5538f8f435
Sara Tamer Bihery <102518852+SaraTamer@users.noreply.github.com>
2022-12-10 22:11:14 +0200

    Update main.cpp

--- SNVector/main.cpp
+++ SNVector/main.cpp
@@ -4,9 +4,12 @@
 int main()
 {
     SNVector<int> v1(3);
-    int arr[3] = { 1, 3 ,5 };
-    SNVector<int> v2(arr, 3);
+    int arr[3] = {1, 3 ,5};
+    SNVector<int> v2(arr , 3);
     SNVector<int> v3(v2);
     //v2 = move(v1);
-    cout << v2;
+    //cout << v2[2];
+    v2.push_back(4);
+    //cout << v2;
+    cout << v2.pop_back();
 }
