3db13480bb6f3c21def6781d041e44aa75e12f03
NourhanE.Hassan <92875566+nouribrahem@users.noreply.github.com>
2022-12-13 23:33:57 +0200

    edit

--- SNVector/main.cpp
+++ SNVector/main.cpp
@@ -4,7 +4,7 @@
 int main()
 {
     SNVector<int> v1(3);
-    int arr[7] = {1, 2 ,3 , 4,5 , 6 , 7};
+    int arr[7] = {1, 2 ,3 , 4, 5 , 6 , 7};
     SNVector<int> v2(arr , 7);
     //SNVector<int> v3(v2);
     //v2 = move(v1);
@@ -18,9 +18,9 @@ int main()
     //v2.push_back(33);
     //cout << v2.Size() << v2.Capacity()<<' ' << v2 << endl;
     //v2.insert(1, 55);
-    v2.insert(7, 45);
-    v2.insert(8, 22);
-    v2.insert(2, 787);
-    cout << v2.Size() << v2.Capacity() << ' ' << v2 << endl;
+    //v2.insert(7, 45);
+    //v2.insert(8, 22);
+    //v2.insert(2, 787);
+    //cout << v2.Size() << v2.Capacity() << ' ' << v2 << endl;
     return 0;
 }
